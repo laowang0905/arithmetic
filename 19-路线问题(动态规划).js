cffee11d7a697b3c8c04244c8fa325b95341e559
laowang0905 <wjd_coder@163.com>
2021-08-30 10:44:31 +0800

    按权重随机选择

--- 19-路线问题(动态规划).js
+++ 19-路线问题(动态规划).js
@@ -13,16 +13,18 @@ var uniquePathsWithObstacles = function(obstacleGrid) {
   for (let i = 0; i < m && obstacleGrid[i][0] == 0; i++) {
       dp[i][0] = 1;
   }
+  
   for (let j = 0; j < n && obstacleGrid[0][j] == 0; j++) {
       dp[0][j] = 1;
   }
   
-  for(let i = 1; i < m; i++){ 
+  for(let i = 1; i < m; i++){
       for(let j = 1; j < n; j++){
           if(obstacleGrid[i][j] === 0){
               dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
           }
       }
   }
+
   return dp[m - 1][n - 1]
 };
