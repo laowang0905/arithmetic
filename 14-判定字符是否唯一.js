3ae317ba702b9c3b2a4d913e5899877cbe8bef81
laowang0905 <wjd_coder@163.com>
2021-09-01 10:18:39 +0800

    2021/9/1

--- 14-判定字符是否唯一.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {string} astr
- * @return {boolean}
- */
-var isUnique = function (astr) {
-  return [...new Set(astr.split(''))].length == astr.length
-};
-
-console.log(isUnique('abc'));
