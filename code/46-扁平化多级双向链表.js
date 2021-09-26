d4be7bab0f847d77cc6d0d59483f9836665c2212
laowang0905 <wjd_coder@163.com>
2021-09-26 14:45:43 +0800

    两整数相加

---
+++ code/46-扁平化多级双向链表.js
@@ -0,0 +1,25 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,prev,next,child) {
+ *    this.val = val;
+ *    this.prev = prev;
+ *    this.next = next;
+ *    this.child = child;
+ * };
+ */
+
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+var flatten = function(head) {
+  const links = head
+  while (links != null) {
+    let child
+    if (links.child) {
+      
+    }
+    links = links.next
+  }
+  links.next
+};
